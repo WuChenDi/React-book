c65a431d7b9f758ef1da8f50dcec4d89674ca387
WuChenDi <996194720@qq.com>
2019-11-15 23:31:14 +0800

    update xx

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,40 +1,67 @@
 import React, { Component, Fragment } from "react";
+import "./index.css";
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputValue: "hello",
-            list: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      list: ["vue", "react", "angular"]
+    };
+  }
 
-    render() {
-        return (
-            <Fragment>
-                <div>
-                    <input
-                        type="text"
-                        value={this.state.inputValue}
-                        onChange={this.handleInputChange.bind(this)}
-                    />
-                    <button>提交</button>
-                </div>
-                <ul>
-                    <li>vue</li>
-                    <li>react</li>
-                </ul>
-            </Fragment>
-        );
-    }
+  render() {
+    return (
+      <Fragment>
+        <div>
+          {/* 注释 */}
+          <input
+            className="input"
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.handleInputChange.bind(this)}
+          />
+          <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+        </div>
+        <ul>
+          {this.state.list.map((item, index) => {
+            return (
+              <li key={index} onClick={this.handleItemDelete.bind(this, index)}>
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+      </Fragment>
+    );
+  }
 
-    handleInputChange(e) {
-        // console.log(this);
-        // console.log(e.target.value);
-        this.setState({
-            inputValue: e.target.value
-        });
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    });
+  }
+
+  handleBtnClick() {
+    if (this.state.inputValue.trim() === "") {
+      alert("请输入内容");
+      return;
     }
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ""
+    });
+  }
+
+  handleItemDelete(index) {
+    // immutable
+    // state 不允许我们做任何的改变
+    const list = [...this.state.list];
+    list.splice(index, 1);
+    this.setState({
+      list
+    });
+  }
 }
 
 export default TodoList;
