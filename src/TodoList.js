090687fee4d51d7c09d1389c8f2c819c5b687c34
WuChenDi <996194720@qq.com>
2019-11-26 09:15:39 +0800

    update redux->todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,38 +1,69 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import "antd/dist/antd.css";
-import { Input, Button, List, Typography } from "antd";
-
-const data = [
-  "Racing car sprays burning fuel into crowd.",
-  "Japanese princess to wed commoner.",
-  "Australian walks 100km after outback crash.",
-  "Man charged over missing wedding girl.",
-  "Los Angeles battles huge wildfires."
-];
+import { Input, Button, List } from "antd";
+import store from "./store/index";
+import {
+  getInputChangeAction,
+  getAddItemAction,
+  getDeleteItemAction
+} from "./store/actionCreators";
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = store.getState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    store.subscribe(this.handleStoreChange);
+    // console.log(this.state);
   }
+
   render() {
     return (
       <div style={{ margin: "10px" }}>
         <div>
           <Input
+            value={this.state.inputValue}
             style={{ width: "300px", marginRight: "10px" }}
             placeholder="todo info"
-          />
-          <Button type="primary">Primary</Button>
+            onChange={this.handleInputChange}
+          ></Input>
+          <Button type="primary" onClick={this.handleBtnClick}>
+            Primary
+          </Button>
         </div>
         <List
           style={{ marginTop: "10px", width: "300px" }}
           bordered
-          dataSource={data}
-          renderItem={item => <List.Item> {item} </List.Item>}
-        />
+          dataSource={this.state.list}
+          renderItem={(item, index) => (
+            <List.Item onClick={this.handleItemDelete.bind(this, index)}>
+              {item}
+            </List.Item>
+          )}
+        ></List>
       </div>
     );
+  }
+
+  handleInputChange(e) {
+    const action = getInputChangeAction(e.target.value);
+    store.dispatch(action);
+  }
+
+  handleStoreChange() {
+    this.setState(store.getState());
+  }
+
+  handleBtnClick() {
+    const action = getAddItemAction();
+    store.dispatch(action);
+  }
+
+  handleItemDelete(index) {
+    const action = getDeleteItemAction(index);
+    store.dispatch(action);
   }
 }
 
