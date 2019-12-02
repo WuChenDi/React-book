4e7407352da8f9b3bc2ac2087496e9d980f61cf3
WuChenDi <996194720@qq.com>
2019-12-02 20:04:29 +0800

    udpate react book

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { fragment } from "react";
+import { GlobalStyled } from "./style.js";
+
+import Header from "./common/header/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <fragment>
+      <Header />
+      <GlobalStyled />
+    </fragment>
+
+    // <div className="App">
+    //   hello world
+    // </div>
   );
 }
 
