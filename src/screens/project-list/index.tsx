61ac68f0ca95241c403aac0e60d23c96632cd2d8
WuChenDi <996194720@qq.com>
2021-07-04 22:12:02 +0800

    feat: 添加登录状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,34 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { SearchPanel } from './search-panel';
-import { List } from './list';
-import qs from 'qs';
-import { cleanObject, useDebounce, useMount } from '../../utils';
+import React, { useEffect, useState } from "react";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import qs from "qs";
+import { cleanObject, useDebounce, useMount } from "../../utils";
+import { useHttp } from "../../utils/http";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
   const [param, setParam] = useState({
-    name: '',
-    personId: ''
+    name: "",
+    personId: ""
   });
   const debouncedParam = useDebounce(param, 200);
   const [list, setList] = useState([]);
+  const client = useHttp();
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async response => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users", ).then(setUsers);
   });
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
 
   return (
